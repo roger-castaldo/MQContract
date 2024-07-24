3c4928a66e35aa1935ce4942d75ba5ae79e80aa7
roger-castaldo <roger.castaldo@gmail.com>
2024-07-24 11:07:40 -0400

    adding testing workflow
    adding in an automatic testing workflow

---
+++ Abstractions/Exceptions.cs
@@ -0,0 +1,16 @@
+﻿namespace MQContract
+{
+    public sealed class InvalidChannelOptionsTypeException
+        : InvalidCastException
+    {
+        public InvalidChannelOptionsTypeException(IEnumerable<Type> expectedTypes,Type recievedType) : 
+            base($"Expected Channel Options of Types[{string.Join(',',expectedTypes.Select(t=>t.FullName))}] but recieved {recievedType.FullName}")
+        {
+        }
+
+        public InvalidChannelOptionsTypeException(Type expectedType, Type recievedType) :
+            base($"Expected Channel Options of Type {expectedType.FullName} but recieved {recievedType.FullName}")
+        {
+        }
+    }
+}
