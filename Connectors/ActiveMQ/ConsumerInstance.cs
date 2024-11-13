69d3c938f79b677f6d0ac74053d6cb9d50175f2f
roger-castaldo <roger.castaldo@gmail.com>
2024-11-13 10:16:58 -0500

    updated ActiveMQ
    cannot have 2 instances of the same group and channel on the same session, so updated code accordingly

---
+++ Connectors/ActiveMQ/ConsumerInstance.cs
@@ -0,0 +1,36 @@
+﻿using Apache.NMS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MQContract.ActiveMQ
+{
+    internal class ConsumerInstance(string channel,string group, IMessageConsumer messageConsumer,Action cleanup)
+    {
+        public string Channel => channel;
+        public string Group =>group;
+
+        private int listenerCount = 1;
+
+        public void AddListener() => listenerCount++;
+
+        public Task<IMessage> ReceiveAsync() => messageConsumer.ReceiveAsync();
+
+        public async Task CloseAsync()
+        {
+            listenerCount--;
+            if (listenerCount == 0)
+            {
+                await messageConsumer.CloseAsync();
+                cleanup();
+            }
+        }
+
+        public void Dispose()
+        {
+            messageConsumer.Dispose();
+        }
+    }
+}
--- Connectors/ActiveMQ/Subscriptions/SubscriptionBase.cs
+++ Connectors/ActiveMQ/Subscriptions/SubscriptionBase.cs
@@ -4,16 +4,13 @@ using MQContract.Interfaces.Service;
 
 namespace MQContract.ActiveMQ.Subscriptions
 {
-    internal class SubscriptionBase(Action<IMessage> messageReceived,Action<Exception> errorReceived,ISession session, string channel,string group) : IServiceSubscription
+    internal class SubscriptionBase(Action<IMessage> messageReceived,Action<Exception> errorReceived, ConsumerInstance consumer) : IServiceSubscription
     {
         private bool disposedValue;
-        private IMessageConsumer? consumer;
         protected readonly CancellationTokenSource cancelToken = new();
-        protected string Channel => channel;
         
         internal async ValueTask StartAsync()
         {
-            consumer = await session.CreateSharedConsumerAsync(SessionUtil.GetTopic(session, channel),group);
             _=Task.Run(async () =>
             {
                 while (!cancelToken.IsCancellationRequested)
