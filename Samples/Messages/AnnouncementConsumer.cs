1235f2a414d2fa3d0b8c213c8a3628e55a26abce
roger-castaldo <roger.castaldo@gmail.com>
2025-02-20 20:15:05 -0500

    added consumer sample
    Added in a sample class to display how to define and register a PubSub consumer

---
+++ Samples/Messages/AnnouncementConsumer.cs
@@ -0,0 +1,18 @@
+﻿using MQContract.Interfaces;
+using MQContract.Interfaces.Consumers;
+
+namespace Messages
+{
+    internal class AnnouncementConsumer : IPubSubConsumer<ArrivalAnnouncement>
+    {
+        void IBaseConsumer.ErrorRecieved(Exception error)
+        {
+            Console.WriteLine($"Announcement error: {error.Message}");
+        }
+
+        void IPubSubConsumer<ArrivalAnnouncement>.MessageReceived(IReceivedMessage<ArrivalAnnouncement> message)
+        {
+            Console.WriteLine($"Announcing the arrival of {message.Message.LastName}, {message.Message.FirstName} in member 2 of the group.. [{message.ID},{message.ReceivedTimestamp}]");
+        }
+    }
+}
