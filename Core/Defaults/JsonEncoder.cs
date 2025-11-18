f0c38cc6b5b35312d1145df2aca47b8907258f1d
Roger Castaldo <roger.castaldo@gmail.com>
2025-11-18 10:51:39 -0500

    changed json encoder
    Adjusted the Json encoder to use a more direct method that the original methodology for performance

--- Core/Defaults/JsonEncoder.cs
+++ Core/Defaults/JsonEncoder.cs
@@ -3,7 +3,7 @@ using System.Text.Json;
 
 namespace MQContract.Defaults
 {
-    internal class JsonEncoder<T> : IMessageTypeEncoder<T>
+    internal class JsonEncoder<TMessage> : IMessageTypeEncoder<TMessage>
     {
         private static JsonSerializerOptions JsonOptions => new()
         {
@@ -14,14 +14,10 @@ namespace MQContract.Defaults
             ReadCommentHandling=JsonCommentHandling.Skip
         };
 
-        public async ValueTask<T?> DecodeAsync(Stream stream)
-            => await JsonSerializer.DeserializeAsync<T>(stream, options: JsonOptions);
+        public async ValueTask<TMessage?> DecodeAsync(Stream stream)
+            => await JsonSerializer.DeserializeAsync<TMessage>(stream, options: JsonOptions);
 
-        public async ValueTask<byte[]> EncodeAsync(T message)
-        {
-            using var ms = new MemoryStream();
-            await JsonSerializer.SerializeAsync<T>(ms, message, options: JsonOptions);
-            return ms.ToArray();
-        }
+        public ValueTask<byte[]> EncodeAsync(TMessage message)
+            => ValueTask.FromResult(JsonSerializer.SerializeToUtf8Bytes<TMessage>(message, JsonOptions));
     }
 }
